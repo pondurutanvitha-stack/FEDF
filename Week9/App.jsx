58cd1e29d9d567c6e2122e5d716284fb187e3505
pondurutanvitha-stack <pondurutanvitha@klh.edu.in>
2026-06-16 18:48:26 +0530

    Create App.jsx

---
+++ Week9/App.jsx
@@ -0,0 +1,12 @@
+import AddBook from "./components/AddBook";
+
+function App() {
+  return (
+    <div>
+      <h1>Library Management System</h1>
+      <AddBook />
+    </div>
+  );
+}
+
+export default App;
